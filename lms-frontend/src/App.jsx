e9840101d43ece048551524ba9746d2296af33d1
Ankur Mishra <99ankurmishra@gmail.com>
2024-11-09 16:22:47 +0530

    Added denied page

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -6,6 +6,7 @@ import {Route,Routes} from 'react-router-dom'
 import Signup from './pages/Signup';
 import Signin from './pages/Signin'
 import Contact from './pages/contact';
+import Denied from './pages/Denied';
 
 function App() {
 
@@ -16,8 +17,9 @@ function App() {
   <Route path='*' element={<Notfound/>}/>
   <Route path='/signup' element={<Signup/>}/>
   <Route path='/contacts' element={<Contact/>}/>
-
   <Route path='/signin' element={<Signin/>}/>
+  <Route path='/denied' element={<Denied/>}/>
+  
 
  </Routes>
   )
